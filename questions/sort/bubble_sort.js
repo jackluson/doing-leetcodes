8a221523d43010b97a3070233078850016c75931
jackluson <18219112108@163.com>
2021-09-08 23:51:31 +0800

    wip: optimize bubble sort

--- questions/sort/bubble_sort.js
+++ questions/sort/bubble_sort.js
@@ -5,11 +5,32 @@ const bubbleSort = (arr) => {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        console.log("arr", arr);
       }
     }
   }
 };
 
-bubbleSort(arr);
+const bubbleSort_op = (arr) => {
+  let last_max_obj = {
+    value: arr[0],
+    ops: 0,
+  };
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = last_max_obj.ops; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        console.log("arr", arr);
+        if (j !== arr.length - i - 2) {
+          last_max_obj.ops = j + 1;
+          last_max_obj.value = arr[j + 1];
+          console.log("last_max_obj", last_max_obj);
+        }
+      }
+    }
+  }
+};
+
+bubbleSort_op(arr);
 
 console.log(arr);
